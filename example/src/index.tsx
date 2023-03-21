85ef565ccd4d2221616dc76da5b1c1befca79968
rishikesh.s <rdshinde2000@gmail.com>
2023-03-21 19:37:28 +0530

    refactore: example

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import { AuthButton, VisualDAuthProvider } from 'react-visual-d-auth'
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <VisualDAuthProvider
-    publicKey={process.env.METAMASK_ACCOUNT_PUBLIC_KEY}
-    privateKey={process.env.METAMASK_ACCOUNT_PRIVATE_KEY}
-    mode='Development'
-    useWindowWallet={false}
-    onErrorHandler={(error) => console.log(error)}
-    onSuccessHandler={(result) => console.log(result)}
-    // children={''}
-  >
-    <div id='auth-form'>
-      <AuthButton />
-    </div>
-  </VisualDAuthProvider>,
+ReactDOM.render(
+  <React.StrictMode>
+    <VisualDAuthProvider
+      publicKey={process.env.METAMASK_ACCOUNT_PUBLIC_KEY}
+      privateKey={process.env.METAMASK_ACCOUNT_PRIVATE_KEY}
+      mode='Development'
+      useWindowWallet={false}
+      onErrorHandler={(error) => console.log(error)}
+      onSuccessHandler={(result) => console.log(result)}
+      // children={''}
+    >
+      <div id='auth-form'>
+        <AuthButton />
+        sdvsdv
+      </div>
+    </VisualDAuthProvider>
+    ,
+  </React.StrictMode>,
+  document.getElementById('root'),
 )

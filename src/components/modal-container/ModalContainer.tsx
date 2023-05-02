80e4d0fc6fedd7c201bc0cdbc0318a7b2424e6f9
rishikesh.s <rdshinde2000@gmail.com>
2023-05-03 01:02:18 +0530

    update: First Successful Publish
    - Its a working publish with tailwindcss

--- src/components/modal-container/ModalContainer.tsx
+++ src/components/modal-container/ModalContainer.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import '../../styles/tailwind.css';
+
 type Props = {
   children: React.ReactNode;
   styles?: React.CSSProperties;
@@ -20,7 +22,7 @@ export const ModalContainer = (props: Props) => {
       exit={{ scale: 0.8, opacity: 0 }}
       transition={{ type: 'spring' }}
       style={{ ...styles }}
-      className={`${className} fixed top-[0%] sm:w-[96%] sm:mx-[2%] sm:left-[0] md:left-[17%] md:min-w-[60%] md:w-[60%] min-h-[50%] transform md:translate-y-[5%] md:translate-x-[18.5%] md:p-5 sm:p-3 xl:p-7 bg-[#fff] m-5 md:h-auto mx-auto rounded-md flex flex-col justify-center items-center duration-900 border-[1px] border-gray-100 shadow-lg shadow-gray-400`}
+      className={`${className} fixed top-[0%] sm:w-[96%] sm:mx-[2%] sm:left-[0] md:left-[9%] md:min-w-[60%] md:w-[60%] min-h-[50%] transform md:translate-y-[5%] md:translate-x-[14%] md:p-5 sm:p-3 xl:p-7 bg-[#fff] m-5 md:h-auto mx-auto rounded-md flex flex-col justify-center items-center duration-900 border-[1px] border-gray-100 shadow-lg shadow-gray-400`}
     >
       {children}
     </motion.div>
